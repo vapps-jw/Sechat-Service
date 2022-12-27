3b41eb2ce30fbea648ca1cda2eab1e42b7004764
JW <jonasz.witek@pm.me>
2022-12-27 13:45:40 +0100

    updates

---
+++ Sechat.Service/Config/DbManager.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sechat.Data;
+
+namespace Sechat.Service.Config
+{
+    public static class DbManager
+    {
+        public static void PrepareDatabase(IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.CreateScope();
+            var context = serviceScope.ServiceProvider.GetService<SechatContext>();
+            if (context is not null)
+            {
+                SeedData(context);
+            }
+        }
+
+        private static void SeedData(SechatContext context)
+        {
+            Console.WriteLine("--> Attempting to apply migrations...");
+            try
+            {
+                context.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"--> Could not run migrations: {ex.Message}");
+            }
+        }
+    }
+}
