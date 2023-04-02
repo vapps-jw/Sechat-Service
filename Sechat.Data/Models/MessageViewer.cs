244a5edb1f98d7fdab342ac09ca3a275cd895407
JW <jonasz.witek@pm.me>
2023-04-02 16:19:14 +0200

    message views wip

---
+++ Sechat.Data/Models/MessageViewer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sechat.Data.Models;
+public record MessageViewer
+{
+    public long Id { get; set; }
+
+    [Required]
+    [MaxLength(36)]
+    public string UserId { get; set; }
+
+    public Message Message { get; set; }
+    public long MessageId { get; set; }
+}
