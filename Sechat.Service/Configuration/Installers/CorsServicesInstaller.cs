cd7c565aa6059013aca73cc9228530691cbfb023
JW <jonasz.witek@pm.me>
2023-05-06 15:45:10 +0200

    cors update

--- Sechat.Service/Configuration/Installers/CorsServicesInstaller.cs
+++ Sechat.Service/Configuration/Installers/CorsServicesInstaller.cs
@@ -10,9 +10,7 @@ public class CorsServicesInstaller : IServiceInstaller
     public void Install(WebApplicationBuilder webApplicationBuilder) =>
         _ = webApplicationBuilder.Services.AddCors(options => options
             .AddPolicy(AppConstants.CorsPolicies.WebClient, build => build
-            .WithOrigins(
-                webApplicationBuilder.Configuration.GetValue("CorsSettings:WebAppUrl", ""),
-                webApplicationBuilder.Configuration.GetValue("CorsSettings:WebAppDomainUrl", ""))
+            .WithOrigins(webApplicationBuilder.Configuration.GetValue("CorsSettings:WebAppUrl", ""))
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials()));
