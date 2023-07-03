b5b785b5474f6313d40752fef5f9bc06f0c36efd
JW <jonasz.witek@pm.me>
2023-07-03 21:56:26 +0200

    redundant endpoint removed

--- Sechat.Service/Controllers/ChatController.cs
+++ Sechat.Service/Controllers/ChatController.cs
@@ -230,21 +230,6 @@ public class ChatController : SechatControllerBase
         return Ok();
     }
 
-    [HttpPatch("direct-messages-viewed")]
-    public async Task<IActionResult> DirectMessagesViewed([FromBody] ResourceId resourceId)
-    {
-        if (!_userRepository.CheckContact(resourceId.Id, UserId, out var contact))
-        {
-            return BadRequest("You cant do that");
-        }
-
-        _chatRepository.MarkDirectMessagesAsViewed(UserId, resourceId.Id);
-        var recipientId = contact.InvitedId.Equals(UserId) ? contact.InviterId : contact.InvitedId;
-        await _chatHubContext.Clients.Group(recipientId).DirectMessagesWereViewed(new DirectMessagesViewed(resourceId.Id));
-
-        return Ok();
-    }
-
     [HttpDelete("message/{roomId}/{messageId}")]
     public async Task<IActionResult> DeleteMessage(string roomId, long messageId)
     {
