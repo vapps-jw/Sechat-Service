813734891179d5805b525b3dfcbb705cbf49ca31
JW <jonasz.witek@pm.me>
2023-08-18 21:16:34 +0200

    405 fixed

--- Sechat.Service/Middleware/CustomResponseHeadersMiddleware.cs
+++ Sechat.Service/Middleware/CustomResponseHeadersMiddleware.cs
@@ -16,12 +16,24 @@ public class CustomResponseHeadersMiddleware : IMiddleware
     {
         try
         {
-            context.Response.Headers.Add("API-RES", "sechat-server-response");
+            context.Response.Headers.Add("SECHAT-RES", "sechat-server-response");
             context.Response.Headers.Add("X-Developed-By", "JWTK");
             context.Response.Headers[HeaderNames.CacheControl] = "no-store";
             //context.Response.Headers[HeaderNames.CacheControl] = "max-age=0,no-cache,must-revalidate";
             //context.Response.Headers[HeaderNames.Expires] = "Tue, 01 Jan 1970 00:00:00 GMT";
             //context.Response.Headers[HeaderNames.Pragma] = "no-cache";
+
+            context.Response.OnStarting(() =>
+            {
+                if (context.Response.StatusCode == 405)
+                {
+                    context.Response.StatusCode = 405;
+                    context.Response.Headers[HeaderNames.CacheControl] = "no-store";
+                    context.Response.Headers.Add("SECHAT-AUTH", "Forbidden");
+                }
+
+                return Task.CompletedTask;
+            });
 
             await next(context);
         }
