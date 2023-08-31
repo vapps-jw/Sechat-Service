b936a3c8f1519a3af633e6911940dcf1222d9189
JW <jonasz.witek@pm.me>
2023-08-31 14:18:58 +0200

    calendar

---
+++ Sechat.Data/Migrations/20230831121852_ReminderIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sechat.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReminderIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Reminders_Reminded",
+                table: "Reminders",
+                column: "Reminded");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reminders_Reminded",
+                table: "Reminders");
+        }
+    }
+}
