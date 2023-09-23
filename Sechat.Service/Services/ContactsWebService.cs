10a9de9892a2abc2e7772e5bbb40e72517e1b648
JW <jonasz.witek@pm.me>
2023-09-23 13:12:13 +0200

    contacts web

---
+++ Sechat.Service/Services/ContactsWebService.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Sechat.Data;
+using System;
+using System.Collections.Generic;
+
+namespace Sechat.Service.Services;
+
+public class ContactsWebService
+{
+    private readonly IDbContextFactory<SechatContext> _contextFactory;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly IMapper _mapper;
+
+    public ContactsWebService(
+        IDbContextFactory<SechatContext> contextFactory,
+        UserManager<IdentityUser> userManager,
+        IMapper mapper)
+    {
+        _contextFactory = contextFactory;
+        _userManager = userManager;
+        _mapper = mapper;
+    }
+
+    public List<ContactSuggestion> CreateContactsuggections(uint level) => throw new NotImplementedException();
+}
+
+public class ContactSuggestion
+{
+    public uint Level { get; set; }
+    public string UserName { get; set; }
+    public string ProfilePicture { get; set; }
+}
