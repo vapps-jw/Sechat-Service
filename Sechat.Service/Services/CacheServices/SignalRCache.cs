d95f29224c0770c60b75de065acfdc960c10241d
JW <jonasz.witek@pm.me>
2023-10-14 12:57:51 +0200

    signalr cache

--- Sechat.Service/Services/CacheServices/SignalRCache.cs
+++ Sechat.Service/Services/CacheServices/SignalRCache.cs
@@ -1,4 +1,6 @@
-﻿using Sechat.Service.Configuration;
+﻿using Microsoft.Extensions.Logging;
+using Sechat.Service.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,10 +10,15 @@ namespace Sechat.Service.Services.CacheServices;
 public class SignalRCache
 {
     private readonly SemaphoreSlim _signalRSemaphore;
+    private readonly ILogger<SignalRCache> _logger;
 
     public Dictionary<string, HashSet<string>> ConnectedUsers { get; set; } = new();
 
-    public SignalRCache() => _signalRSemaphore = new SemaphoreSlim(1);
+    public SignalRCache(ILogger<SignalRCache> logger)
+    {
+        _signalRSemaphore = new SemaphoreSlim(1);
+        _logger = logger;
+    }
 
     public string IsUserOnline(string userId) =>
         ConnectedUsers.ContainsKey(userId) ? AppConstants.ContactState.Online : AppConstants.ContactState.Offline;
@@ -20,22 +27,47 @@ public class SignalRCache
 
     public async Task AddUser(string userId, string connectionId)
     {
-        await _signalRSemaphore.WaitAsync();
-        if (ConnectedUsers.ContainsKey(userId))
+        try
         {
-            _ = ConnectedUsers[userId].Add(connectionId);
+            await _signalRSemaphore.WaitAsync();
+            if (ConnectedUsers.ContainsKey(userId))
+            {
+                _ = ConnectedUsers[userId].Add(connectionId);
+            }
+            else
+            {
+                ConnectedUsers.Add(userId, new HashSet<string>() { connectionId });
+            }
+            _ = _signalRSemaphore.Release();
         }
-        ConnectedUsers.Add(userId, new HashSet<string>() { connectionId });
-        _ = _signalRSemaphore.Release();
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"{nameof(SignalRCache)} {nameof(AddUser)} {ex.GetType()}");
+        }
+        finally
+        {
+            _ = _signalRSemaphore.Release();
+        }
     }
 
     public async Task RemoveUserConnection(string userId, string connectionId)
     {
-        await _signalRSemaphore.WaitAsync();
-        if (ConnectedUsers.ContainsKey(userId))
+        try
         {
-            _ = ConnectedUsers[userId].Remove(connectionId);
+            await _signalRSemaphore.WaitAsync();
+            if (ConnectedUsers.ContainsKey(userId))
+            {
+                _ = ConnectedUsers[userId].Remove(connectionId);
+            }
+            _ = _signalRSemaphore.Release();
         }
-        _ = _signalRSemaphore.Release();
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"{nameof(SignalRCache)} {nameof(RemoveUserConnection)} {ex.GetType()}");
+        }
+        finally
+        {
+            _ = _signalRSemaphore.Release();
+        }
     }
 }
